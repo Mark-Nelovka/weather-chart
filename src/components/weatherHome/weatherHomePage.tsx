f40afc911f8e5c57bfb5ca073a1b64297aa8fab5
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2022-12-23 13:48:20 +0200

    ended ability card
    - changed animation
    - fix styles
    - added subTitle with day, year
    - added subTitle with description temperature

--- src/components/weatherHome/weatherHomePage.tsx
+++ src/components/weatherHome/weatherHomePage.tsx
@@ -1,20 +1,14 @@
-import {
-  Grid,
-  Box,
-  Card,
-  Paper,
-  Typography,
-  Icon,
-  Button,
-} from "@mui/material";
+import { Grid, Box, Paper, Typography, Button } from "@mui/material";
+import { Container } from "@mui/system";
+import { Link, useLocation } from "react-router-dom";
 import Header from "../Header";
-import Rain from "../../images/water.png";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import Brightness3Icon from "@mui/icons-material/Brightness3";
 import s from "./weatHome.module.css";
-import { Container } from "@mui/system";
 
 export const WeatherHomePage = () => {
+  const { pathname } = useLocation();
+
   return (
     <>
       <Header title="Weather home page" />
@@ -23,59 +17,63 @@ export const WeatherHomePage = () => {
           <Box sx={{ width: "100%" }}>
             <Grid container rowSpacing={2} columnSpacing={{ md: 5 }}>
               <Grid item md={3}>
-                <Paper
-                  className={s.card}
-                  sx={{
-                    backgroundColor: "#64b5f6",
-                  }}
-                  elevation={2}
-                >
-                  <Typography
-                    sx={{
-                      mb: 2,
-                      textAlign: "center",
-                      fontFamily: "Raleway",
-                      fontSize: 24,
-                      fontWeight: "bold",
-                      color: "#fff",
-                    }}
-                  >
-                    Kyiv
+                <Paper className={s.cardRain} elevation={2}>
+                  <Typography className={s.cardTitle}>Kyiv</Typography>
+                  <Typography className={s.cardSubTitle}>
+                    Monday: 23.12.22
                   </Typography>
                   <Box className={s.containerInfo}>
                     <Box className={s.containerItemsInfo}>
                       <Typography component={"span"}>-2</Typography>
+                      <Typography
+                        sx={{
+                          mt: 1,
+                          fontFamily: "Raleway",
+                          fontWeight: "bold",
+                          letterSpacing: 1.2,
+                          fontSize: 12,
+                          color: "#fff",
+                        }}
+                      >
+                        Min
+                      </Typography>
                       <Box
+                        className={s.animationContainer}
                         sx={{
                           backgroundColor: "#64b5f6",
-                          width: 80,
-                          height: 45,
-                          mb: 2,
-                          mt: 2,
                         }}
-                      ></Box>
-                      {/* <Box className={s.sun1}>{<WbSunnyIcon />}</Box> */}
+                      >
+                        <Box className={s.sun1}>{<WbSunnyIcon />}</Box>
+                      </Box>
                       <Typography component={"span"}>7:00</Typography>
                     </Box>
                     <Box className={s.containerItemsInfo}>
                       <Typography component={"span"}>5</Typography>
-                      {/* <Box className={s.animationContainer}> */}
-                      {/* <Box className={s.sun2}>{<Brightness3Icon />}</Box> */}
-                      <Box
+                      <Typography
                         sx={{
-                          backgroundColor: "#0f52ba",
-                          width: 80,
-                          height: 45,
-                          mb: 2,
-                          mt: 2,
+                          mt: 1,
+                          fontFamily: "Raleway",
+                          fontWeight: "bold",
+                          letterSpacing: 1.2,
+                          fontSize: 12,
+                          color: "#fff",
                         }}
-                      ></Box>
-                      {/* </Box> */}
+                      >
+                        Max
+                      </Typography>
+                      <Box
+                        className={s.animationContainer}
+                        sx={{ backgroundColor: "#0f52ba" }}
+                      >
+                        <Box className={s.sun2}>{<Brightness3Icon />}</Box>
+                      </Box>
                       <Typography component={"span"}>18:00</Typography>
                     </Box>
                   </Box>
                   <Box className={s.containerButtonInfo}>
-                    <Button variant="contained">More info</Button>
+                    <Link to={`${pathname}/Kyiv/details`}>
+                      <Button variant="contained">More info</Button>
+                    </Link>
                   </Box>
                 </Paper>
               </Grid>
